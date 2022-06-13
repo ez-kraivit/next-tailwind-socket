595ad71dc3e5917f4feee1f159e2f68a3f1a8bd0
kraivit <kraivit@edvisory.co.th>
2022-06-13 23:01:42 +0700

    [Create] Next Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
+
+import Layout from '../components/layout'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
 
-export default MyApp
+// export default MyApp
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+  <>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </>
+  )
+}
